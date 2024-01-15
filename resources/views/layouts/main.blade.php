f12fbaa5b518f2c434c80b0285eef641a2e3180a
mhmmdriz <rizki25030@gmail.com>
2024-01-15 22:23:39 +0700

    delete unnecessary plugins

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -10,10 +10,7 @@
   <!-- Font Awesome -->
   <link rel="stylesheet" href="/lte/plugins/fontawesome-free/css/all.min.css">
   <!-- Ionicons -->
-  {{-- <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css"> --}}
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
-  <!-- Tempusdominus Bootstrap 4 -->
-  <link rel="stylesheet" href="/lte/plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css">
   <!-- Theme style -->
   <link rel="stylesheet" href="/lte/dist/css/adminlte.min.css">
   <!-- overlayScrollbars -->
@@ -71,29 +68,11 @@
 </script>
 <!-- Bootstrap 4 -->
 <script src="/lte/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
-<!-- ChartJS -->
-<script src="/lte/plugins/chart.js/Chart.min.js"></script>
-<!-- Sparkline -->
-<script src="/lte/plugins/sparklines/sparkline.js"></script>
-<!-- JQVMap -->
-<script src="/lte/plugins/jqvmap/jquery.vmap.min.js"></script>
-<script src="/lte/plugins/jqvmap/maps/jquery.vmap.usa.js"></script>
-<!-- jQuery Knob Chart -->
-<script src="/lte/plugins/jquery-knob/jquery.knob.min.js"></script>
-<!-- daterangepicker -->
-<script src="/lte/plugins/moment/moment.min.js"></script>
-<script src="/lte/plugins/daterangepicker/daterangepicker.js"></script>
-<!-- Tempusdominus Bootstrap 4 -->
-<script src="/lte/plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js"></script>
-<!-- Summernote -->
-<script src="/lte/plugins/summernote/summernote-bs4.min.js"></script>
+
 <!-- overlayScrollbars -->
 <script src="/lte/plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js"></script>
 <!-- AdminLTE App -->
 <script src="/lte/dist/js/adminlte.js"></script>
-<!-- AdminLTE for demo purposes -->
-<script src="/lte/dist/js/demo.js"></script>
-<!-- AdminLTE dashboard demo (This is only for demo purposes) -->
-<script src="/lte/dist/js/pages/dashboard.js"></script>
+
 </body>
 </html>
